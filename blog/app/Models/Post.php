d7929c3d87dc942ffd30dac3a0b9d65b127a3ef1
dina103333 <121552009+dina103333@users.noreply.github.com>
2024-04-19 22:58:41 +0200

    Update relation rollback

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -10,6 +10,6 @@ class Post extends Model
     use HasFactory;
     protected $guarded=[];
     public function reviews(){
-        return $this->belongsTo(Review::class);
+        return $this->hasMany(Review::class);
     }
 }
